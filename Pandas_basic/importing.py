67b328bbcdc98ef42c3066e253c76672476611da
Utrino <utkarshbhaskar10@gmail.com>
2026-01-08 01:13:49 +0530

    done

--- Pandas_basic/importing.py
+++ Pandas_basic/importing.py
@@ -3,4 +3,4 @@ df = pd.read_csv("data/aadhaar_data.csv")
 # df = pd.read_json(file_path.json)
 print(df)# this shows the overview of our file
 
-print(df.to_string()) # DON'T RUN FOR LARHE FILES, it will give every thing
+# print(df.to_string()) # DON'T RUN FOR LARGE FILES, it will give every thing
